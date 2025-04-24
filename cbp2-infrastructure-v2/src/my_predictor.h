4e5218912e16b16dc22fc6a802696c59cf4fa6ad
Fuyao Zhou <147701356+FuyaoZhou@users.noreply.github.com>
2025-04-23 23:58:47 -0700

    Update my_predictor.h, use Dual-Perceptron Branch Predictor, 4.512

--- cbp2-infrastructure-v2/src/my_predictor.h
+++ cbp2-infrastructure-v2/src/my_predictor.h
@@ -1,52 +1,79 @@
-// my_predictor.h
-// This file contains a sample my_predictor class.
-// It is a simple 32,768-entry gshare with a history length of 15.
-// Note that this predictor doesn't use the whole 32 kilobytes available
-// for the CBP-2 contest; it is just an example.
+#include <cstdint>
+#include <cmath>
+#include <cstring>
 
 class my_update : public branch_update {
 public:
-	unsigned int index;
+    int y_long, y_short, y_combined;
+    int index_long, index_short;
 };
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	15
-#define TABLE_BITS	15
-	my_update u;
-	branch_info bi;
-	unsigned int history;
-	unsigned char tab[1<<TABLE_BITS];
+    static constexpr int LONG_HIST = 48;
+    static constexpr int SHORT_HIST = 12;
+    static constexpr int TABLE_BITS = 10;
+    static constexpr int THRESHOLD = 90;
 
-	my_predictor (void) : history(0) { 
-		memset (tab, 0, sizeof (tab));
-	}
+    my_update u;
+    branch_info bi;
 
-	branch_update *predict (branch_info & b) {
-		bi = b;
-		if (b.br_flags & BR_CONDITIONAL) {
-			u.index = 
-				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
-				^ (b.address & ((1<<TABLE_BITS)-1));
-			u.direction_prediction (tab[u.index] >> 1);
-		} else {
-			u.direction_prediction (true);
-		}
-		u.target_prediction (0);
-		return &u;
-	}
+    int long_hist[LONG_HIST] = {};
+    int short_hist[SHORT_HIST] = {};
 
-	void update (branch_update *u, bool taken, unsigned int target) {
-		if (bi.br_flags & BR_CONDITIONAL) {
-			unsigned char *c = &tab[((my_update*)u)->index];
-			if (taken) {
-				if (*c < 3) (*c)++;
-			} else {
-				if (*c > 0) (*c)--;
-			}
-			history <<= 1;
-			history |= taken;
-			history &= (1<<HISTORY_LENGTH)-1;
-		}
-	}
+    int8_t table_long[1 << TABLE_BITS][LONG_HIST + 1] = {};
+    int8_t table_short[1 << TABLE_BITS][SHORT_HIST + 1] = {};
+
+    branch_update *predict(branch_info &b) {
+        bi = b;
+
+        u.index_long = b.address & ((1 << TABLE_BITS) - 1);
+        u.index_short = (b.address ^ (b.address >> 2)) & ((1 << TABLE_BITS) - 1);
+
+        int y1 = table_long[u.index_long][0];
+        int y2 = table_short[u.index_short][0];
+        for (int i = 0; i < LONG_HIST; i++)
+            y1 += table_long[u.index_long][i + 1] * long_hist[i];
+        for (int i = 0; i < SHORT_HIST; i++)
+            y2 += table_short[u.index_short][i + 1] * short_hist[i];
+
+        u.y_long = y1;
+        u.y_short = y2;
+        u.y_combined = y1 + y2;
+
+        u.direction_prediction(u.y_combined >= 0);
+        u.target_prediction(0);
+        return &u;
+    }
+
+    void update(branch_update *u_, bool taken, unsigned int target) {
+        if (!(bi.br_flags & BR_CONDITIONAL)) return;
+        my_update *u = (my_update *)u_;
+        int t = taken ? 1 : -1;
+
+        if ((u->y_combined >= 0) != taken || std::abs(u->y_combined) < THRESHOLD) {
+            int8_t *w_long = table_long[u->index_long];
+            w_long[0] = clip(w_long[0] + t);
+            for (int i = 0; i < LONG_HIST; i++)
+                w_long[i + 1] = clip(w_long[i + 1] + t * long_hist[i]);
+
+            int8_t *w_short = table_short[u->index_short];
+            w_short[0] = clip(w_short[0] + t);
+            for (int i = 0; i < SHORT_HIST; i++)
+                w_short[i + 1] = clip(w_short[i + 1] + t * short_hist[i]);
+        }
+
+        for (int i = LONG_HIST - 1; i > 0; i--) long_hist[i] = long_hist[i - 1];
+        long_hist[0] = t;
+
+        for (int i = SHORT_HIST - 1; i > 0; i--) short_hist[i] = short_hist[i - 1];
+        short_hist[0] = t;
+    }
+
+private:
+    int8_t clip(int val) {
+        if (val > 127) return 127;
+        if (val < -127) return -127;
+        return val;
+    }
 };
